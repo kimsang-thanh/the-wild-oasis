7420a13ce3f5c021e5da6347bea6be6c17555c46
kimsang <kimthclo@gmail.com>
2025-11-22 11:51:56 +0700

    implement home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} antialiased text-(--primary-100) bg-(--primary-950)  min-h-screen  flex flex-col`}
+        className={`${josefin.className} antialiased text-(--primary-100) bg-(--primary-950)  min-h-screen  flex flex-col `}
       >
         <Header />
         <div className="flex-1 px-8 py-12">
---
+++ tailwind.config.ts
@@ -0,0 +1,41 @@
+// tailwind.config.ts
+import { type Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "var(--primary-50)",
+          100: "var(--primary-100)",
+          200: "var(--primary-200)",
+          300: "var(--primary-300)",
+          400: "var(--primary-400)",
+          500: "var(--primary-500)",
+          600: "var(--primary-600)",
+          700: "var(--primary-700)",
+          800: "var(--primary-800)",
+          900: "var(--primary-900)",
+          950: "var(--primary-950)",
+        },
+        accent: {
+          50: "var(--accent-50)",
+          100: "var(--accent-100)",
+          200: "var(--accent-200)",
+          300: "var(--accent-300)",
+          400: "var(--accent-400)",
+          500: "var(--accent-500)",
+          600: "var(--accent-600)",
+          700: "var(--accent-700)",
+          800: "var(--accent-800)",
+          900: "var(--accent-900)",
+          950: "var(--accent-950)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
